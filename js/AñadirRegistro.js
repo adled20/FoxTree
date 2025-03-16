65c30f6157ae74a0215d8e2e3840d789afa96c0d
adled20 <javier.doria@cecar.edu.co>
2025-03-16 00:06:08 -0500

    Grafica

---
+++ js/AñadirRegistro.js
@@ -0,0 +1,95 @@
+import {Acceso} from "./Conexion.js"
+import { EncontrarRegistro } from './GraficaMain.js';
+import { DibujarGrafica } from "./GraficaMain.js";
+import { collection, query, where, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+// Declaracion de variables
+const Gasto = document.getElementById("Gasto");
+const Ingreso = document.getElementById("Ingreso");
+var tipo = "Gastos";
+const u=document.createElement("u");
+const UID=sessionStorage.getItem("UID");
+
+const AñadirRegistro=document.getElementById("send");
+
+
+
+
+Lista(tipo); 
+Gasto.addEventListener("click", () => {
+    tipo = "Gastos";
+    u.textContent="Gastos"
+    console.log(tipo);
+    Gasto.textContent="";
+    Gasto.appendChild(u)
+    Ingreso.textContent="Ingresos"
+    Lista(tipo)
+   
+});
+
+Ingreso.addEventListener("click", () => {
+    tipo = "Ingresos";
+    console.log(tipo);
+      u.innerText="Ingresos"
+      Ingreso.textContent="";
+    Ingreso.appendChild(u)
+  
+    Gasto.textContent="Gastos"
+    Lista(tipo)
+    
+});
+
+async function Lista(tipo) {
+    const Etiqueta = collection(Acceso, "Etiqueta");
+    const ListBox = document.getElementById("ListBox");
+    ListBox.innerHTML = ""; 
+    const Eti = query(Etiqueta, where("Tipo", "==", tipo));
+   
+    const ListaEtiquetas = await getDocs(Eti);
+    var contador=1;
+    ListaEtiquetas.forEach((doc) => {
+const etiqueta=doc.data();
+const option = document.createElement("option");
+option.textContent=""+etiqueta.Nombre;
+
+option.style.backgroundColor=etiqueta.Color
+ListBox.appendChild(option);
+contador = contador +1;
+
+});
+  }
+
+ document.getElementById("InicioUsuario").addEventListener("submit", function(evt){
+     evt.preventDefault();
+     const Cantidad=document.getElementById("Cantidad").value;
+const ListSelected=document.getElementById("ListBox").value;
+const Comentario=document.getElementById("comentario").value;
+const fecha=new Date();
+     if (!Cantidad || !ListSelected || !Comentario) {
+        
+     } else {
+         
+     EnviarDatos(Cantidad,ListSelected,fecha, Comentario)
+     }
+ 
+    async function EnviarDatos(Cantidad,ListSelected, fecha, Comentario) {
+        try{
+        
+         const docRef = await addDoc(collection(Acceso, "Registros"), {
+            Cantidad: parseInt(Cantidad),
+            Fecha: fecha,
+            NombreEtiqueta: ListSelected,
+            Tipo: tipo,
+            UID: UID,
+            URLimagen:null,
+            Comentario:Comentario
+          });
+         const nuevosDatos=await EncontrarRegistro();
+         DibujarGrafica(nuevosDatos);
+             console.log("Registrado")
+
+        } catch(error){
+            console.log("Error al registrar:", error.code, error.message);
+         }
+        
+     }
+ })
---
+++ js/GraficaMain.js
@@ -0,0 +1,111 @@
+import {Acceso} from "../js/Conexion.js"
+import { collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+
+const UID = sessionStorage.getItem("UID")
+var tipo = "Gastos";
+
+
+ga.addEventListener("click", () => {
+    tipo = "Gastos";
+    DibujarGrafica(datos)
+   
+});
+
+Gasto.addEventListener("click", () => {
+    tipo = "Gastos";
+   
+});
+
+Ingreso.addEventListener("click", () => {
+    tipo = "Ingresos";
+    
+});
+
+    async function EncontrarRegistro() {
+        const datos =[];
+      const Registro = collection(Acceso, "Registros");
+      const q = query(Registro, where("UID", "==", UID), where("Tipo","==",tipo));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const registro = doc.data();
+       const  etiqueta=registro.NombreEtiqueta
+        const Valor=registro.Cantidad
+        if (datos[etiqueta]) {
+            datos[etiqueta]+=Valor;
+        } else {
+            datos[etiqueta]=Valor;
+            
+        }
+      
+      });
+      const datosAgrupados= Object.keys(datos).map(function(etiqueta){
+        return {
+            name:etiqueta,
+            value:datos[etiqueta],
+        };
+      });
+      return datosAgrupados;
+    }
+    export { EncontrarRegistro };
+const datos=await EncontrarRegistro();
+Grafica(datos);
+
+export function DibujarGrafica(DatosNuevos) {
+   Grafica(DatosNuevos);
+}
+
+function Grafica(datos) {
+    var dom = document.getElementById('grafica');
+var myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    top: '5%',
+    left: 'center'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '70%'],
+      avoidLabelOverlap: false,
+      padAngle: 5,
+      itemStyle: {
+        borderRadius: 10
+      },
+      label: {
+        show: false,
+        position: 'center'
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: 'bold'
+        }
+      },
+      labelLine: {
+        show: false
+      },
+      data: datos
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
+}
+
